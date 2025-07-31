190f1b7a491f606c3c4d5e665491e5dacbc91043
HarukaDev <harukakasugano31@gmail.com>
2025-07-31 13:31:50 -0500

    fix

--- database/migrations/2025_07_31_160102_add_foreign_keys_sql_to_productos_importados_excel_table.php
+++ database/migrations/2025_07_31_160102_add_foreign_keys_sql_to_productos_importados_excel_table.php
@@ -15,10 +15,7 @@ class AddForeignKeysSqlToProductosImportadosExcelTable extends Migration
     public function up()
     {
         // Agregar foreign key para entidad_id usando SQL directo
-        DB::statement('ALTER TABLE productos_importados_excel ADD CONSTRAINT fk_productos_entidad FOREIGN KEY (entidad_id) REFERENCES bd_entidades_reguladoras(id) ON DELETE SET NULL ON UPDATE CASCADE');
-        
-        // Agregar foreign key para tipo_etiquetado_id usando SQL directo
-        DB::statement('ALTER TABLE productos_importados_excel ADD CONSTRAINT fk_productos_tipo_etiquetado FOREIGN KEY (tipo_etiquetado_id) REFERENCES bd_productos(id) ON DELETE SET NULL ON UPDATE CASCADE');
+      
     }
 
     /**
