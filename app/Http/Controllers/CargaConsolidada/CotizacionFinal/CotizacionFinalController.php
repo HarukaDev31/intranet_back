d1be1f622ec7540465cd5b33cbdf93db1dee5210
Haruka31Dev <harukakasugano31@gmail.com>
2025-09-18 14:58:23 -0500

    fix

--- app/Http/Controllers/CargaConsolidada/CotizacionFinal/CotizacionFinalController.php
+++ app/Http/Controllers/CargaConsolidada/CotizacionFinal/CotizacionFinalController.php
@@ -863,7 +863,7 @@ class CotizacionFinalController extends Controller
                 $carga = $contenedor->carga;
                 $fechaArribo = $contenedor->fecha_arribo;
                 $telefono = preg_replace('/\s+/', '', $cotizacion->telefono);
-                $this->phoneNumberId = $telefono ? $telefono . '@c.us' : '';
+                $this->phoneNumberId = '51912705923'? '51912705923'. '@c.us' : '';
     
                 $message = "📦 *Consolidado #" . $carga . "*\n" .
                     "Hola " . $nombre . " 😁 un gusto saludarte! \n" .
