b45f561317e1254bbe230d9e4dc4d276c4c32af7
HarukaDev31 <harukakasugano31@gmail.com>
2025-09-25 17:45:58 -0500

    add getEntregasDetalle updateStatusEntrega and getDelivery function on EntregaController

--- app/Http/Controllers/CargaConsolidada/EntregaController.php
+++ app/Http/Controllers/CargaConsolidada/EntregaController.php
@@ -26,7 +26,10 @@ class EntregaController extends Controller
         $query = DB::table('contenedor_consolidado_cotizacion as CC')
             ->join('carga_consolidada_contenedor as C', 'C.id', '=', 'CC.id_contenedor')
             ->select('C.*','CC.*')
-            ->where('CC.id_contenedor', $idContenedor);
+            ->where('CC.id_contenedor', $idContenedor)
+            ->whereNotNull('CC.estado_cliente')
+            ->whereNull('CC.id_cliente_importacion')
+            ->where('CC.estado_cotizador', 'CONFIRMADO');
         // Aplicar filtro de estado si se proporciona
         $page = $request->input('currentPage', 1);
         $perPage = $request->input('itemsPerPage', 100);
@@ -70,7 +73,10 @@ class EntregaController extends Controller
         $query = DB::table('contenedor_consolidado_cotizacion as CC')
             ->join('carga_consolidada_contenedor as C', 'C.id', '=', 'CC.id_contenedor')
             ->select('C.*','CC.*')
-            ->where('CC.id_contenedor', $idContenedor);
+            ->where('CC.id_contenedor', $idContenedor)
+            ->whereNotNull('CC.estado_cliente')
+            ->whereNull('CC.id_cliente_importacion')
+            ->where('CC.estado_cotizador', 'CONFIRMADO');
         // Aplicar filtro de estado si se proporciona
         $page = $request->input('currentPage', 1);
         $perPage = $request->input('itemsPerPage', 100);
@@ -104,8 +110,58 @@ class EntregaController extends Controller
             ]
         ]);
     }
-    public function getEntregasDetalle($idEntrega)
+    public function getDelivery(Request $request, $idContenedor)
+    {
+        // Lo obtiene de la tabla de clientes asociados al contenedor
+        $query = DB::table('contenedor_consolidado_cotizacion as CC')
+            ->join('carga_consolidada_contenedor as C', 'C.id', '=', 'CC.id_contenedor')
+            ->select('C.*','CC.*')
+            ->where('CC.id_contenedor', $idContenedor)
+            ->whereNotNull('CC.estado_cliente')
+            ->whereNull('CC.id_cliente_importacion')
+            ->where('CC.estado_cotizador', 'CONFIRMADO');
+            // Aplicar filtro de estado si se proporciona
+        $page = $request->input('currentPage', 1);
+        $perPage = $request->input('itemsPerPage', 100);
+        $clientes = $query->paginate($perPage, ['*'], 'currentPage', $page);
+        // Aplicar filtros adicionales si se proporcionan
+        if ($request->has('search')) {
+            $search = $request->search;
+            $query->where(function ($q) use ($search) {
+                $q->where('CC.nombre', 'LIKE', "%{$search}%")
+                    ->orWhere('CC.documento', 'LIKE', "%{$search}%")
+                    ->orWhere('CC.correo', 'LIKE', "%{$search}%");
+            });
+        }
+        // Ordenamiento
+        $sortField = $request->input('sort_by', 'CC.id');
+        $sortOrder = $request->input('sort_order', 'asc');
+
+        // Paginación
+        $data = $query->paginate($perPage, ['*'], 'page', $page);
+
+        return response()->json([
+            'data' => $data->items(),
+            'success' => true,
+            'pagination' => [
+                'current_page' => $data->currentPage(),
+                'per_page' => $data->perPage(),
+                'total' => $data->total(),
+                'last_page' => $data->lastPage(),
+                'from' => $data->firstItem(),
+                'to' => $data->lastItem()
+            ]
+        ]);
+    }
+    public function getEntregasDetalle($idContenedor)
     {
         // Lógica para obtener los detalles de una entrega específica
+        $entrega = DB::table('contenedor_consolidado_cotizacion')
+            ->where('id', $idContenedor)
+            ->first();
+        if (!$entrega) {
+            return response()->json(['message' => 'Entrega no encontrada'], 404);
+        }
+        return response()->json(['data' => $entrega, 'success' => true]);
     }
 }
