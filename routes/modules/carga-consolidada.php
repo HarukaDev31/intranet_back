06c6d1a0da4f48448d8fc1337eb35079dc9ea6af
HarukaDev <harukakasugano31@gmail.com>
2025-10-03 19:02:07 -0500

    fix

--- routes/modules/carga-consolidada.php
+++ routes/modules/carga-consolidada.php
@@ -226,6 +226,7 @@ Route::group(['prefix' => 'carga-consolidada', 'middleware' => 'jwt.auth'], func
     // Pagos
     Route::group(['prefix' => 'pagos'], function () {
         Route::get('consolidado', [PagosController::class, 'getConsolidadoPagos']);
+        Route::get('consolidado/delivery/{idCotizacion}', [PagosController::class, 'getDetailsPagosConsolidadoDelivery']);
         Route::get('consolidado/{idCotizacion}', [PagosController::class, 'getDetailsPagosConsolidado']);
         Route::put('consolidado/saveStatus/{idPago}', [PagosController::class, 'actualizarPagoCoordinacion']);
         Route::put('consolidado/updateNota/{idCotizacion}', [PagosController::class, 'updateNotaConsolidado']);
