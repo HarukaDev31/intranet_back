28272cf5ea64f896a1bed326a55cfdd5faee5925
HarukaDev <harukakasugano31@gmail.com>
2025-11-12 18:05:58 -0500

    fiX

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Events\ConnectionEstablished;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,20 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-
-    public function boot(): void
+    public function boot()
     {
-        DB::purge('mysql'); // Limpia caché de conexión
-        DB::reconnect('mysql'); // Reabre conexión con el timezone correcto
-
-        DB::connection('mysql')->setQueryGrammar(DB::connection('mysql')->getQueryGrammar());
-        DB::connection('mysql')->setPostProcessor(DB::connection('mysql')->getPostProcessor());
-
-        try {
-            DB::connection('mysql')->statement("SET time_zone = '-05:00'");
-            Log::info('Zona horaria MySQL configurada correctamente (-05:00)');
-        } catch (\Throwable $e) {
-            Log::warning('No se pudo establecer zona horaria MySQL: ' . $e->getMessage());
-        }
+        //
     }
 }
