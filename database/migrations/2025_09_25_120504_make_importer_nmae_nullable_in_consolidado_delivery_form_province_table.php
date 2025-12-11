a0b4bc01a770566c301080e80091bf57ebc9dcd9
Haruka31Dev <harukakasugano31@gmail.com>
2025-12-11 09:48:14 -0500

    fix

---
+++ database/migrations/2025_09_25_120504_make_importer_nmae_nullable_in_consolidado_delivery_form_province_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class MakeImporterNmaeNullableInConsolidadoDeliveryFormProvinceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('consolidado_delivery_form_province', function (Blueprint $table) {
+            $table->string('importer_nmae')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('consolidado_delivery_form_province', function (Blueprint $table) {
+            $table->string('importer_nmae')->nullable(false)->change();
+        });
+    }
+}
