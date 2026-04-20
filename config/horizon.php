f711759d01aa5d2aabe6d17c597c3ebf6c0819b0
Francis Torres <harukakasugano31@gmail.com>
2026-04-20 12:29:23 -0500

    facturacion data module

--- config/horizon.php
+++ config/horizon.php
@@ -235,6 +235,19 @@ return [
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
+            'supervisor-importaciones-facturacion' => [
+                'connection' => 'redis',
+                'queue' => ['importaciones_facturacion'],
+                'balance' => 'auto',
+                'maxProcesses' => 2,
+                'maxTime' => 0,
+                'maxJobs' => 0,
+                'memory' => 128,
+                'tries' => 1,
+                'timeout' => 180,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
         ],
         'local' => [
 
