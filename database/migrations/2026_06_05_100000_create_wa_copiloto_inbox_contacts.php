94ea573fd2fb08643d115d992326eb9321617848
Haruka31Dev <harukakasugano31@gmail.com>
2026-06-04 18:48:28 -0500

    fix

--- database/migrations/2026_06_05_100000_create_wa_copiloto_inbox_contacts.php
+++ database/migrations/2026_06_05_100000_create_wa_copiloto_inbox_contacts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWaCopilotoInboxContactsTable20260605 extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -36,4 +36,4 @@ class CreateWaCopilotoInboxContactsTable20260605 extends Migration
     {
         Schema::dropIfExists('wa_copiloto_inbox_contacts');
     }
-}
+};
